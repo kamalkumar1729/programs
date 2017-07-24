4e8321d8f6b26250cafa047c5411bdec463fbf08
kamalkumar1729 <kamalkumar1729@gmail.com>
2017-07-23 23:57:22 -0700

    working program

--- fileread.c
+++ fileread.c
@@ -19,28 +19,29 @@ void * read_and_count(void *file)
  //  printf("kamal\n");
    thread_data *data_ptr = (thread_data*)file;
 
-  printf("path in thread %s\n",data_ptr->name);
+  //printf("path in thread %s\n",data_ptr->name);
 
    FILE *fp=fopen(data_ptr->name,"r");
-   printf("after fp\n");
+  // printf("after fp\n");
 
-   char buff[10];
+   char buff[100];
    //set number of lines in a file, to Zero.
    data_ptr->linecount=0;
 
 if(fp != NULL)
 {
-   while(fgets(buff,10,fp) != NULL)
+   while(fgets(buff,100,fp) != NULL)
    {
+    //  printf("fiel line content %s\n",buff );
       (data_ptr->linecount) ++;
-      printf("linen in iteration %d\n",data_ptr->linecount );
+      //printf("linen in iteration %d\n",data_ptr->linecount );
     }
 
 }else
   printf("file doesn't exist! NULL file pointer.  \n");
 
 
-  printf("#lines %d\n",data_ptr->linecount );
+  printf("lines in %s file are %d\n",data_ptr->name, data_ptr->linecount );
 
 }
 
@@ -69,17 +70,26 @@ int main()
         strcat(full_path,dir->d_name);
         strcpy(th_ptr->name,full_path);
 
-         printf("path is %s\n",full_path );
+        // printf("path is %s\n",full_path );
 
         if( ! (pthread_create(&thread_id[iter],NULL,read_and_count,th_ptr)) );
-            printf("thread creatted with id %d\n",iter);
+            //printf("thread creatted with id %d\n",iter);
 
 
         iter++;
 
  }
 
+printf("%d\n",iter );
 
- sleep(2);
+
+
+  //wait for all the threads to finish
+  for(int i=0; i<iter; i++)
+        pthread_join(thread_id[i],NULL);
+
+
+
+ //sleep(2);
 
 }
