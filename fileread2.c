973b8fcda82c3fe36deb7862924af0afed92c106
kamalkumar1729 <kamalkumar1729@gmail.com>
2017-07-25 18:35:02 +0530

    mideified

--- fileread2.c
+++ fileread2.c
@@ -4,7 +4,6 @@
 #include<string.h>
 #include<sys/types.h>
 #include<dirent.h>
-#include<errno.h>
 
 //structure for holding single thread data
 typedef struct _thread_data{
@@ -25,7 +24,6 @@ void * read_and_count(void *file)
 
   //set number of lines in a file, to Zero.
   data_ptr->linecount=0;
-  printf("file path in thread %s\n",data_ptr->name );
 
   if(fp == NULL)
   {
@@ -42,10 +40,6 @@ void * read_and_count(void *file)
     }
   }
   printf("%s    %d \n",data_ptr->name, data_ptr->linecount );
-
-  //close the file
-  fclose(fp);
-
 }
 
 
@@ -54,17 +48,14 @@ int main()
   //thread ids
   pthread_t thread_id[10];
 
-  char current_dir[]="./textfies/";
+  char current_dir[]="./textfiles/";
 
   DIR *directory;
   struct dirent *dir;
 
   directory=opendir(current_dir);
 
-//check if diectory exist
-if(directory){
-
-  thread_data *th_ptr[100];
+  thread_data *th_ptr[50];
   int iter=0;
 
   while((dir=readdir(directory)) != NULL){
@@ -74,42 +65,28 @@ if(directory){
 
     strcpy(full_path,current_dir);
     strcat(full_path,dir->d_name);
-    strcpy(th_ptr[iter]->name,full_path);
+    strcpy(th_ptr->name,full_path);
 
-   printf("path is %s\n",full_path );
-
-    if((pthread_create(&thread_id[iter],NULL,read_and_count,th_ptr[iter]))!= 0 ){
-         printf("Unbale to create thread\n");
+    // printf("path is %s\n",full_path );
+    if((pthread_create(&thread_id[iter],NULL,read_and_count,th_ptr))!= 0 ){
+         printf("Unbale to crate thread\n");
          pthread_exit(NULL);
     }
+    //printf("thread creatted with id %d\n",iter);
 
-    //printf("thread created with id %d\n",iter);
     iter++;
  }
+  //printf("total files %d\n",iter );
 
   //wait for all the threads to finish
  for(int i=0; i<iter; i++)
     pthread_join(thread_id[i],NULL);
 
+
   //free the memory
  for(int j=0; j<iter; j++)
-    free(th_ptr[j]);
+        free(th_ptr[j]);
 
 
-printf("%d error number \n",errno );
-
-}else if(ENOENT == errno){
-   printf("Directory doesn't Exist. error no: %s\n",strerror(errno));
-   perror("kamal");
-}else{
-  printf("Can't open directory\n");
-
-}
-
-// errno=0;
-// printf("%d error number \n",errno );
-// int x=7,y=0;
-// int z=x/y;
-// printf("%d error number \n",errno );
 return 0;
 }
